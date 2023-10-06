8a72b07ccc88afe1916cfaaa7fbf42b24408bb32
Daniel Tschertkow <daniel.tschertkow@posteo.de>
2023-10-06 11:43:48 +0200

    Config dump

--- dot-config/zsh/aliases.zsh
+++ dot-config/zsh/aliases.zsh
@@ -12,3 +12,4 @@ alias jrn='journalctl --user --follow --this-boot --unit'
 alias sys='systemctl --user'
 alias ip='ip --color=auto'
 alias rcom='sudo picocom --baud 115200 --escape t /dev/ttyUSB0'
+alias pacman-package-fzf="pacman -Qq | fzf --preview 'pacman -Qil {}' --layout=reverse --bind 'enter:execute(pacman -Qil {} | less)'"
